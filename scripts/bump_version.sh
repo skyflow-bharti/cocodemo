5e9f978e89757deffa20660f061de53d7b012756
skyflow-bharti <bharti.sagar@skyflow.com>
2023-04-26 20:17:37 +0530

    workflow

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -2,7 +2,7 @@ version=$1
 SEMVER=$version
 
 sed -E "s/s.version .+/s.version      = \"$SEMVER\"/g" "./cocodemo.podspec" > tempfile
-sed -E "s/source .+/source       = { :git => \"https:\/\/github.com\/skyflow-bharti\/skyflow-iOS.git\", :tag => \"$1\" }/g" tempfile > ./cocodemo.podspec && rm -f tempfile
+sed -E "s/source .+/source       = { :git => \"https:\/\/github.com\/skyflow-bharti\/cocodemo.git\", :tag => \"$1\" }/g" tempfile > ./cocodemo.podspec && rm -f tempfile
 
 echo --------------------------
 echo "Done, Pod now at v$1"
