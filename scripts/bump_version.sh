ef6ff9bdaa7bd804ce6d1ba9c97f10daa4182945
skyflow-bharti <bharti.sagar@skyflow.com>
2023-05-18 10:07:06 +0530

    add internal release

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,9 +1,22 @@
 version=$1
 SEMVER=$version
 
-sed -E "s/s.version .+/s.version      = \"$SEMVER\"/g" "./cocodemo.podspec" > tempfile
-sed -E "s/source .+/source       = { :git => \"https:\/\/github.com\/skyflow-bharti\/cocodemo.git\", :tag => \"$1\" }/g" tempfile > ./cocodemo.podspec && rm -f tempfile
-awk -v semver="$SEMVER" '{gsub(/var SDK_VERSION = ".+"/, "var SDK_VERSION = \"" semver "\"")}1' "./cocodemo/Classes/Version.swift" > tempfile && mv tempfile "./cocodemo/Classes/Version.swift"
+if [ -z $2 ]
+then
 
-echo --------------------------
-echo "Done, Pod now at v$1"
+    sed -E "s/s.version .+/s.version      = \"$SEMVER\"/g" "./cocodemo.podspec" > tempfile
+    sed -E "s/source .+/source       = { :git => \"https:\/\/github.com\/skyflow-bharti\/cocodemo.git\", :tag => \"$1\" }/g" tempfile > ./cocodemo.podspec && rm -f tempfile
+    awk -v semver="$SEMVER" '{gsub(/var SDK_VERSION = ".+"/, "var SDK_VERSION = \"" semver "\"")}1' "./cocodemo/Classes/Version.swift" > tempfile && mv tempfile "./cocodemo/Classes/Version.swift"
+
+    echo --------------------------
+    echo "Done, Pod now at v$1"
+
+else
+
+    sed -E "s/s.version .+/s.version      = \"$SEMVER-dev.$2\"/g" "./cocodemo.podspec" > tempfile
+    sed -E "s/source .+/source       = { :git => \"https:\/\/github.com\/skyflow-bharti\/cocodemo.git\", :commit => \"$2\" }/g" tempfile > ./cocodemo.podspec && rm -f tempfile
+    awk -v semver="\"$SEMVER-dev.$2\"/g" '{gsub(/var SDK_VERSION = ".+"/, "var SDK_VERSION = \"" semver "\"")}1' "./cocodemo/Classes/Version.swift" > tempfile && mv tempfile "./cocodemo/Classes/Version.swift"
+
+    echo --------------------------
+    echo "Done, Pod now at v$1-dev.$2"
+fi
